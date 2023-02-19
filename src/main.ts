3d538d512eb8493c37e439b9c1ea15d97c22bc59
horrible little slime <69secret69email69@gmail.com>
2023-02-18 20:46:59 -0500

    Accept player name or id

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,38 @@
 import { Args } from "grimoire-kolmafia";
-import { getClanName, print } from "kolmafia";
-import { $item, $items, SlimeTube } from "libram";
+import { getClanName, myId, myName, print } from "kolmafia";
+import { $item, $items, getPlayerFromIdOrName, Player, SlimeTube } from "libram";
+
+function parsePlayer(input: string): Player {
+  const isPlayerId = !/[^\d]/.test(input);
+  const parsedIdOrName = isPlayerId ? parseInt(input) : input;
+  return getPlayerFromIdOrName(parsedIdOrName);
+}
 
 const args = Args.create("distro", "A script for AltEnd to distribute his tubes", {
-  slimeling: Args.string({
-    key: "slimeling",
-    default: "Phreddrickkv2",
-  }),
-  skills: Args.string({
-    key: "skills",
-    default: "AltEnd",
-  }),
-  others: Args.string({
-    key: "others",
-    default: "AltEnd",
-  }),
+  slimeling: Args.custom<Player>(
+    {
+      key: "slimeling",
+      default: { name: "Phreddrickkv2", id: 1515124 },
+    },
+    parsePlayer,
+    "Player"
+  ),
+  skills: Args.custom<Player>(
+    {
+      key: "skills",
+      default: { name: myName(), id: parseInt(myId()) },
+    },
+    parsePlayer,
+    "Player"
+  ),
+  others: Args.custom<Player>(
+    {
+      key: "others",
+      default: { name: myName(), id: parseInt(myId()) },
+    },
+    parsePlayer,
+    "Player"
+  ),
 });
 
 export function main(argstring = ""): void {
@@ -25,11 +43,11 @@ export function main(argstring = ""): void {
     }, any skills to ${args.skills}, and everything else to ${args.others}`
   );
 
-  SlimeTube.distribute(args.slimeling, $item`squirming Slime larva`, true);
+  SlimeTube.distribute(args.slimeling.name, $item`squirming Slime larva`, true);
   SlimeTube.distribute(
-    args.skills,
+    args.skills.name,
     $items`slime-soaked sweat gland, slime-soaked hypophysis, slime-soaked brain`,
     true
   );
-  SlimeTube.distribute(args.others, SlimeTube.findLoot(), true);
+  SlimeTube.distribute(args.others.name, SlimeTube.findLoot(), true);
 }
